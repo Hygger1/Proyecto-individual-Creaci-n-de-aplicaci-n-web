ef2927c47a4e21d8bf952ee7c3e82536be50e6f5
Hygger1 <oscar.solano.oesa@gmail.com>
2022-11-30 18:28:47 -0600

    first commit

---
+++ json/question.js
@@ -0,0 +1,52 @@
+data = [
+    {
+        question:"¿Primer pais en ganar un mundial?",
+        answers:{
+            a:"México",
+            b:"Brasil",
+            c:"Italia",
+            d:"Uruguay"
+        },
+        correctAnswer:"d"
+    },
+    {
+        question: "¿Cuál es el país con mas mundiales ganados?",
+        answers:{
+            a:"Brasil",
+            b:"Francia",
+            c:"Inglaterra",
+            d:"Italia"
+        },
+        correctAnswer:"a"
+    },
+    {
+        question: "¿Cuál es el país con mayor participaciones en mundiales?",
+        answers:{
+            a:"Alemania",
+            b:"Brasil",
+            c:"Argentina",
+            d:"Italia"
+        },
+        correctAnswer:"b"
+    },
+    {
+        question: "¿Qué país nunca ha ido a un  mundial?",
+        answers:{
+            a:"Fiyi",
+            b:"Tahiti",
+            c:"Finlandia",
+            d:"Todas las anteriores"
+        },
+        correctAnswer:"d"
+    },
+    {
+        question: "¿Cuantas selecciones participan en el mundial?",
+        answers:{
+            a:"28",
+            b:"30",
+            c:"32",
+            d:"40"
+        },
+        correctAnswer:"c"
+    }
+];
